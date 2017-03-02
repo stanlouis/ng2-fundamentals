9905148ead0cc6c2693f239208a5ce4d82d6350b
Stanley Louis <stanlouis@msn.com>
2017-03-01 16:52:16 -0800

    creating first component

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by stan on 3/1/17.
+ */
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstragitpModule(AppModule);
