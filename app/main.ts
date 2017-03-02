4805465eb306c3ff0d5af842ab824cbbad86256b
Stanley Louis <stanlouis@msn.com>
2017-03-01 18:25:00 -0800

    creating our first Data-bound Component

--- app/main.ts
+++ app/main.ts
@@ -4,4 +4,4 @@
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstragitpModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule);
